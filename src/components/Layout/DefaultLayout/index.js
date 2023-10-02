298439f080f7bf7ecdda29ffaaba3aa11741f8f8
toannguyenit <toannguyen.hcmute@gmail.com>
2023-10-02 17:12:20 +0700

    Dung khung DefaultLayout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,19 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
         <div>
-            <Header />
-            <div className="container">
-                <Sidebar />
-                <div className="content">{children}</div>
+            <div className={cx('wrapper')}>
+                <Header />
+                <div className={cx('container')}>
+                    <Sidebar />
+                    <div className={cx('content')}>{children}</div>
+                </div>
             </div>
         </div>
     );
